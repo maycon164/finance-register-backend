cd3534a516c51be3ab8f435d461088e248760af3
maycon.huanca <maycon.huanca@e-core.com>
2025-03-24 10:12:04 -0300

    add structure

---
+++ src/main/java/com/project/finance/FinanceRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.project.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/finance/FinanceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
