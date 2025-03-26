a4424969f9bf22db22dcebcecd7e0c8f4fdcad49
maycon.huanca <maycon.huanca@e-core.com>
2025-03-26 15:03:08 -0300

    add controller

---
+++ src/main/java/com/project/finance/infra/adapters/inbound/RegisterController.java
@@ -0,0 +1,29 @@
+package com.project.finance.infra.adapters.inbound;
+
+import com.project.finance.core.dto.GetRegistersDTO;
+import com.project.finance.core.model.Register;
+import com.project.finance.core.services.RegisterService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/registers")
+public class RegisterController {
+
+    private final RegisterService registerService;
+
+    public RegisterController(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
+    @GetMapping
+    public List<Register> getRegisters(@ModelAttribute GetRegistersDTO getRegistersDTO) {
+        System.out.println(getRegistersDTO);
+        return registerService.getRegisters(getRegistersDTO);
+    }
+
+}
